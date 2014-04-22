c44c8e78daae1f51668dfaafce0c476505d8bfa1
Maciek Swat <maciekswat@gmail.com>
2014-04-22 13:07:01 -0400

    added ufc

---
+++ share/ufc/UFCConfig.cmake
@@ -0,0 +1,26 @@
+# - UFC
+# CMake configuration for UFC (http:://fenicsproject.org)
+#
+# This file has been automatically generated.
+
+# FIXME: Check that naming conforms to CMake standards
+
+# Package found
+set(UFC_FOUND TRUE)
+
+# Include directories
+set(UFC_INCLUDE_DIRS "D:/dolfin_local_git/include")
+
+# Python include directories
+set(UFC_PYTHON_INCLUDE_DIRS
+  "c:\Python27\include"
+  "d:/source-46/boost/include")
+
+# Python libraries
+set(UFC_PYTHON_LIBRARIES "C:/Python27/libs/libpython27.a")
+
+# Version
+set(UFC_VERSION_STRING "2.3.0")
+
+# The location of the UseUFC.cmake file
+set(UFC_USE_FILE "D:/dolfin_local_git/D:/dolfin_local_git/share/ufc/UseUFC.cmake")
